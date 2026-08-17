c9bbfead1b9ea65b3d50a14b6c8f4eab572d89bf
Alexander Suter <alexander.suter@axonivy.com>
2026-08-17 09:39:49 +0200

    Sonar: Remove unused imports

--- cxf-client-codegen/src/test/java/com/axonivy/ivy/tool/cxf/codegen/model/TestServiceModelWriter.java
+++ cxf-client-codegen/src/test/java/com/axonivy/ivy/tool/cxf/codegen/model/TestServiceModelWriter.java
@@ -1,11 +1,11 @@
 package com.axonivy.ivy.tool.cxf.codegen.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static com.axonivy.ivy.tool.cxf.codegen.TestCxfClientCodegen.generateResource;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -20,24 +20,24 @@ class TestServiceModelWriter {
     Path service = tmpDir.resolve("service.json");
     var model = Files.readString(service);
     assertThat(model)
-      .as("Store name and ports for config/webservice-clients.yaml")
-      .isEqualTo("""
-        {
-          "service" : "com.acme.echo.IvyEchoService",
-          "ports" : {
-            "IvyEchoServiceHttpsSoap11Endpoint" : "https://test-webservices.ivyteam.io:8443/axis2/services/IvyEchoService.IvyEchoServiceHttpsSoap11Endpoint/",
-            "IvyEchoServiceHttpEndpoint" : "http://test-webservices.ivyteam.io:8080/axis2/services/IvyEchoService.IvyEchoServiceHttpEndpoint/",
-            "IvyEchoServiceHttpsSoap12Endpoint" : "https://test-webservices.ivyteam.io:8443/axis2/services/IvyEchoService.IvyEchoServiceHttpsSoap12Endpoint/",
-            "IvyEchoServiceHttpSoap12Endpoint" : "http://test-webservices.ivyteam.io:8080/axis2/services/IvyEchoService.IvyEchoServiceHttpSoap12Endpoint/",
-            "IvyEchoServiceHttpsEndpoint" : "https://test-webservices.ivyteam.io:8443/axis2/services/IvyEchoService.IvyEchoServiceHttpsEndpoint/",
-            "IvyEchoServiceHttpSoap11Endpoint" : "http://test-webservices.ivyteam.io:8080/axis2/services/IvyEchoService.IvyEchoServiceHttpSoap11Endpoint/"
-          }
-        }\
-        """);
+        .as("Store name and ports for config/webservice-clients.yaml")
+        .isEqualTo("""
+          {
+            "service" : "com.acme.echo.IvyEchoService",
+            "ports" : {
+              "IvyEchoServiceHttpsSoap11Endpoint" : "https://test-webservices.ivyteam.io:8443/axis2/services/IvyEchoService.IvyEchoServiceHttpsSoap11Endpoint/",
+              "IvyEchoServiceHttpEndpoint" : "http://test-webservices.ivyteam.io:8080/axis2/services/IvyEchoService.IvyEchoServiceHttpEndpoint/",
+              "IvyEchoServiceHttpsSoap12Endpoint" : "https://test-webservices.ivyteam.io:8443/axis2/services/IvyEchoService.IvyEchoServiceHttpsSoap12Endpoint/",
+              "IvyEchoServiceHttpSoap12Endpoint" : "http://test-webservices.ivyteam.io:8080/axis2/services/IvyEchoService.IvyEchoServiceHttpSoap12Endpoint/",
+              "IvyEchoServiceHttpsEndpoint" : "https://test-webservices.ivyteam.io:8443/axis2/services/IvyEchoService.IvyEchoServiceHttpsEndpoint/",
+              "IvyEchoServiceHttpSoap11Endpoint" : "http://test-webservices.ivyteam.io:8080/axis2/services/IvyEchoService.IvyEchoServiceHttpSoap11Endpoint/"
+            }
+          }\
+          """);
   }
 
   @Test
-  void optIn(@TempDir Path tmpDir, @TempDir Path tmpDir2) throws Exception{
+  void optIn(@TempDir Path tmpDir, @TempDir Path tmpDir2) throws Exception {
     boolean serviceInfo = false;
     generate(tmpDir, serviceInfo);
     assertThat(tmpDir.resolve("service.json")).doesNotExist();
@@ -49,10 +49,10 @@ class TestServiceModelWriter {
 
   private void generate(Path tmpDir, boolean serviceInfo) throws Exception {
     var opts = new CxfClientGenerator.CodegenOpts(
-      "com.acme.echo", Map.of(), false, 
-      serviceInfo);
+        "com.acme.echo", Map.of(), false,
+        serviceInfo);
     new CxfClientGenerator(tmpDir).generate(
-      TestCxfClientCodegen.class.getResource("IvyEchoService.WSDL").toString(), opts);
+        TestCxfClientCodegen.class.getResource("IvyEchoService.WSDL").toString(), opts);
   }
 
 }
